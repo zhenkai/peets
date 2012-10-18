d8f09ccd09e4a4795bc347a66092d826d34b7f8e
Zhenkai Zhu <zhenkai@cs.ucla.edu>
2012-10-17 19:23:33 -0700

    ccnxsocket works

--- backend/ccnxsocket.py
+++ backend/ccnxsocket.py
@@ -8,6 +8,8 @@ class CcnxSocket(object):
   ''' A socket like handler for ccnx operations.
       Runs a simple event loop and handles set interest filter, send interest,
       and publish data. 
+      Current only one ccnx handle is used, we can use multiple handles if needed,
+      but there is no such need as of now.
   '''
 
   __logger = Logger.get_logger('CcnxSocket')
@@ -18,7 +20,6 @@ class CcnxSocket(object):
     self.ccnx_key_locator = pyccn.KeyLocator(self.ccnx_key)
     self.ccnx_handle = CCN() 
     self.event_loop = EventLoop(self.ccnx_handle)
-    start_new_thread(event_loop.run) 
 
   def get_signed_info(self, freshness):
     si = pyccn.SignedInfo()
@@ -33,8 +34,46 @@ class CcnxSocket(object):
     co.name = Name(name)
     co.content = content
 
-    si = get_signed_info(freshness)
+    si = self.get_signed_info(freshness)
     co.signedInfo = si
 
     co.sign(self.ccnx_key)
     self.ccnx_handle.put(co)
+
+  def send_interest(self, name, closure, template = None):
+    self.ccnx_handle.expressInterest(name, closure, template)
+    
+  def register_prefix(self, prefix, closure):
+    self.ccnx_handle.setInterestFilter(prefix, closure)
+
+  def start(self):
+    start_new_thread(self.event_loop.run, ())
+
+  def stop(self):
+    self.event_loop.stop()
+
+
+if __name__ == '__main__':
+  sock1 = CcnxSocket()
+  sock2 = CcnxSocket()
+  sock1.start()
+  sock2.start()
+
+  name = Name('/local/test')
+  content = 'Hello, world!'
+
+  class TestClosure(Closure):
+    def __init__(self):
+      super(TestClosure, self).__init__()
+
+    def upcall(self, kind, upcallInfo):
+#      global UPCALL_CONTENT, RESULT_OK
+      if kind == pyccn.UPCALL_CONTENT:
+        print upcallInfo.ContentObject.content
+
+      return pyccn.RESULT_OK
+
+  sock1.publish_content(name, content, 100)
+  sock2.send_interest(name, TestClosure())
+  from time import sleep
+  sleep(2)
