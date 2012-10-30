cc15d862feade8ae6e8d8d94aa63979bc2af823c
Zhenkai Zhu <zhenkai@cs.ucla.edu>
2012-10-29 23:49:43 -0700

    server serves static files

--- backend/server.py
+++ backend/server.py
@@ -1,25 +1,31 @@
 import sys
- 
 from twisted.internet import reactor
+from twisted.internet.protocol import DatagramProtocol
+from twisted.web.server import Site
+from twisted.web.static import File
 from twisted.python import log
- 
 from autobahn.websocket import WebSocketServerFactory, \
                                WebSocketServerProtocol, \
                                listenWS
- 
- 
+
 class EchoServerProtocol(WebSocketServerProtocol):
- 
-   def onMessage(self, msg, binary):
-      print "sending echo:", msg
-      self.sendMessage(msg, binary)
- 
+  def onMessage(self, msg, binary):
+    print "sending echo:", msg
+    self.sendMessage(msg, binary)
+
+#class EchoUDP(DatagramProtocol):
+#  def datagramReceived(self, data, (host, port)):
+#    print "received %r from %s: %d" % (data, host, port)
+#    self.transport.write(data, (host, port))
+
 if __name__ == '__main__':
- 
-   log.startLogging(sys.stdout)
- 
-   factory = WebSocketServerFactory("ws://localhost:9000", debug = False)
-   factory.protocol = EchoServerProtocol
-   listenWS(factory)
- 
-   reactor.run()
+  log.startLogging(sys.stdout)
+  factory = WebSocketServerFactory("ws://localhost:9000", debug = False)
+  factory.protocol = EchoServerProtocol
+  listenWS(factory)
+  reactor.listenUDP(55935, EchoUDP())
+
+  resource = File('frontend')
+  factory = Site(resource)
+  reactor.listenTCP(8888, factory)
+  reactor.run()
