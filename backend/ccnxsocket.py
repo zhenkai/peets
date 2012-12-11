38eb54305540340fc9d909ca2c37606b6ef8ce43
Zhenkai Zhu <zhenkai@cs.ucla.edu>
2012-12-10 17:36:31 -0800

    doc ccnxsocket

--- backend/ccnxsocket.py
+++ backend/ccnxsocket.py
@@ -3,14 +3,44 @@ import pyccn
 from thread import start_new_thread
 import select
 from log import Logger
+'''
+.. module:: CcnxLoop
+   : platform: Mac OS X, Linux
+   : synopsis: A loop that runs ccn_run.
+
+.. moduleauthor:: Zhenkai Zhu < zhenkai@cs.ucla.edu>
+
+.. module:: CcnxSocket
+   : platform: Mac OS X, Linux
+   : synopsis: A socket like interface for interacting with PyCCN.
+
+.. moduleauthor:: Zhenkai Zhu < zhenkai@cs.ucla.edu>
+
+.. module:: PeetsClosure
+   : platform: Mac OS X, Linux
+   : synopsis: A common closure class so user only needs to define the callback. Closure is required by PyCCN.
+
+.. moduleauthor:: Zhenkai Zhu < zhenkai@cs.ucla.edu>
+'''
 
 class CcnxLoop(object):
+  ''' A loop that runs ccn_run
+  This is going to be scheduled in a separate thread
+  '''
   def __init__(self, handle, *args, **kwargs):
+    '''
+    Args:
+      handle (PyCCN.CCN): the handle to be used in the loop
+    '''
     super(CcnxLoop, self).__init__()
     self.handle = handle
     self.running = False
 
   def run(self):
+    '''
+    Start the loop; use select.select to watch the handle for input and output;
+    runs ccn_run if there is input/output or select timeout
+    '''
     self.running = True
     # select.poll() is disabled in Mac OS X.. dafaq?!
     #poller = select.poll()
@@ -34,6 +64,9 @@ class CcnxLoop(object):
         pass
 
   def stop(self):
+    '''
+    Stop the loop
+    '''
     self.running = False
   
 
@@ -48,6 +81,9 @@ class CcnxSocket(object):
   __logger = Logger.get_logger('CcnxSocket')
 
   def __init__(self, *args, **kwargs):
+    '''
+    Creates a socket. As of now, we try to get the ccnx key from the default location
+    '''
     super(CcnxSocket, self).__init__()
     self.ccnx_key = CCN.getDefaultKey()
     self.ccnx_key_locator = pyccn.KeyLocator(self.ccnx_key)
@@ -55,6 +91,14 @@ class CcnxSocket(object):
     self.event_loop = CcnxLoop(self.ccnx_handle)
 
   def get_signed_info(self, freshness):
+    '''
+    Get signed info to be included in the Content Object
+    Args:
+      freshness (int): the freshness of the Content Object in seconds
+
+    Returns:
+      a PyCCN.SignedInfo object 
+    '''
     si = pyccn.SignedInfo()
     si.publisherPublicKeyDigest = self.ccnx_key.publicKeyID
     si.type = pyccn.CONTENT_DATA
@@ -63,6 +107,14 @@ class CcnxSocket(object):
     return si
 
   def get_pyccn_name(self, name):
+    '''Get a valid name for PyCCN. This is useful when the name string is encoded as unicode, as is the usual case in Python. However, PyCCN has problem handling unicode names, raising TypeError as a result.
+    
+    Args:
+      name (str): the name string
+    
+    Returns:
+      An ascii encoded name string
+    '''
     if isinstance(name, unicode):
       return Name(name.encode('ascii', 'ignore'))
     else:
@@ -70,6 +122,15 @@ class CcnxSocket(object):
     
 
   def publish_content(self, name, content, freshness = 5):
+    '''Publish the data as a Content Object
+    
+    Args:
+      name (str): the name string
+      content (bytes): the data bytes
+
+    Kwargs:
+      freshness (int): the freshness in seconds for the Content Object
+    '''
     co =  ContentObject()
     co.name = self.get_pyccn_name(name)
     co.content = content
@@ -81,32 +142,61 @@ class CcnxSocket(object):
     self.ccnx_handle.put(co)
 
   def send_interest(self, name, closure, template = None):
+    '''Send Interest
+
+    Args:
+      name (str): the name string
+      closure (PyCCN.Closure): the closure that includes the callbacks to be used by PyCCN for this Interest
+
+    Kwargs:
+      template (PyCCN.Interest): the template for the additional field to be carried in the Interest, such as ChildSelector, Lifetime, AnswerOrigin, etc..
+    '''
     n = self.get_pyccn_name(name)
     self.ccnx_handle.expressInterest(n, closure, template)
     
   def register_prefix(self, prefix, closure):
+    '''Register the prefix under which the user wishes to receive Interests
+
+    Args:
+      prefix (str): the prefix name string
+      closure (PyCCN.Closure): the closure that includes the callbacks to be used by PyCCN when an Interest with such prefix comes
+    '''
     p = self.get_pyccn_name(prefix)
     self.ccnx_handle.setInterestFilter(p, closure)
 
   def start(self):
+    '''Start the CcnxLoop
+    '''
     start_new_thread(self.event_loop.run, ())
 
   def stop(self):
+    '''Stop the CcnxLoop
+    '''
     self.event_loop.stop()
 
 class PeetsClosure(Closure):
   ''' A closure for processing PeetsMessage content object
-  timeout_callback should return some ccnx upcall return value
 
   Note: If the subclass of pyccn.Closure is a inner class of some class, it would make ccn_run fail in py-chronos. The reason is unknown. I guess something fishy is happening when the pyccn c code try to call the closure upcall method when the closure class is not resolvable in global name space.
   '''
   def __init__(self, incoming_interest_callback = None, msg_callback = None, timeout_callback = None):
+    '''Customize the PyCCN.Closure subclass
+    Kwargs:
+      incoming_interest_callback: the callback function to be used by PyCCN when an Interest for this closure comes; takes PyCCN.UpcallInfo.Interest as the input
+      msg_callback: the callback function to be used by PyCCN when an ContentObject is fetched; takes PyCCN.UpcallInfo.Interest and PyCCN.UpcallInfo.ContentObject as inputs
+      timeout_callback: the callback function to be used by PyCCN when the Interest times out; takes PyCCN.UpcallInfo.Interest as the input
+    
+    *Note* that *timeout_callback* should return some ccnx upcall return value, e.g. pyccn.UPCALL_REEXPRESS is the user wants the Interest to be re-expressed
+    '''
     super(PeetsClosure, self).__init__()
     self.incoming_interest_callback = incoming_interest_callback
     self.msg_callback = msg_callback
     self.timeout_callback = timeout_callback
 
   def upcall(self, kind, upcallInfo):
+    '''Override the upcall function of the base class
+    This function will be used by PyCCN whenever there is an upcall event
+    '''
     if kind == pyccn.UPCALL_CONTENT:
       if self.msg_callback is not None:
         self.msg_callback(upcallInfo.Interest, upcallInfo.ContentObject)
@@ -119,70 +209,70 @@ class PeetsClosure(Closure):
       
     return pyccn.RESULT_OK
 
-if __name__ == '__main__':
-  from time import sleep, time
-  sock1 = CcnxSocket()
-  sock2 = CcnxSocket()
-  sock1.start()
-  sock2.start()
-
-  name = '/local/test1'
-  content = 'Hello, world!'
-  
-
-  class TestClosure(Closure):
-    def __init__(self):
-      super(TestClosure, self).__init__()
-      self.requested_seq = 0
-      self.fetched_seq = 0
-      
-    def upcall(self, kind, upcallInfo):
-#      print "In upcall, kind = " + str(kind)
-      if kind == pyccn.UPCALL_CONTENT:
-        print 'Got %s: %s' % (upcallInfo.ContentObject.name, upcallInfo.ContentObject.content)
-        name = upcallInfo.ContentObject.name
-        seq = int (str(name).split('/')[-1])
-        self.fetched_seq = seq
-        
-
-      return pyccn.RESULT_OK
-
-
-  sock1.publish_content(name, content, 200)
-
-  import thread  
-  print 'start testing pre-fetching'
-  prefix = '/local/pre/fetch'
-  closure = TestClosure()
-
-  # Note that we use different socks for sending interest and publishing data
-  # this is because, if we use only one sock, the pending interests sent by this sock
-  # would not be satisfied by the content published later by this sock
-  # however, if the content is published earlier than the interest is sent, then
-  # using one sock has no problem
-  def fetch():
-    counter = 0
-    while True:
-      if closure.requested_seq - closure.fetched_seq < 5:
-        name = prefix + '/' + str(counter)
-        closure.requested_seq = counter
-        counter += 1
-        sock2.send_interest(name, closure)
-        
-      sleep(0.1)
-
-  def publish():
-    counter = 0
-    while True:
-      name = prefix + '/' + str(counter)
-      counter += 1
-      content = '%s' % time()
-      sock1.publish_content(name, content, 5)
-      sleep(0.2)
-
-  thread.start_new_thread(fetch, ())
-      
-  sleep(2.5)
-  publish()
-  
-  print "Stopped fetching process"
+#if __name__ == '__main__':
+#  from time import sleep, time
+#  sock1 = CcnxSocket()
+#  sock2 = CcnxSocket()
+#  sock1.start()
+#  sock2.start()
+#
+#  name = '/local/test1'
+#  content = 'Hello, world!'
+#  
+#
+#  class TestClosure(Closure):
+#    def __init__(self):
+#      super(TestClosure, self).__init__()
+#      self.requested_seq = 0
+#      self.fetched_seq = 0
+#      
+#    def upcall(self, kind, upcallInfo):
+##      print "In upcall, kind = " + str(kind)
+#      if kind == pyccn.UPCALL_CONTENT:
+#        print 'Got %s: %s' % (upcallInfo.ContentObject.name, upcallInfo.ContentObject.content)
+#        name = upcallInfo.ContentObject.name
+#        seq = int (str(name).split('/')[-1])
+#        self.fetched_seq = seq
+#        
+#
+#      return pyccn.RESULT_OK
+#
+#
+#  sock1.publish_content(name, content, 200)
+#
+#  import thread  
+#  print 'start testing pre-fetching'
+#  prefix = '/local/pre/fetch'
+#  closure = TestClosure()
+#
+#  # Note that we use different socks for sending interest and publishing data
+#  # this is because, if we use only one sock, the pending interests sent by this sock
+#  # would not be satisfied by the content published later by this sock
+#  # however, if the content is published earlier than the interest is sent, then
+#  # using one sock has no problem
+#  def fetch():
+#    counter = 0
+#    while True:
+#      if closure.requested_seq - closure.fetched_seq < 5:
+#        name = prefix + '/' + str(counter)
+#        closure.requested_seq = counter
+#        counter += 1
+#        sock2.send_interest(name, closure)
+#        
+#      sleep(0.1)
+#
+#  def publish():
+#    counter = 0
+#    while True:
+#      name = prefix + '/' + str(counter)
+#      counter += 1
+#      content = '%s' % time()
+#      sock1.publish_content(name, content, 5)
+#      sleep(0.2)
+#
+#  thread.start_new_thread(fetch, ())
+#      
+#  sleep(2.5)
+#  publish()
+#  
+#  print "Stopped fetching process"
